f82da38a64e765b2f4cb579518b635f8fe685760
Bogdan Pahomov <36045528+VaultHack@users.noreply.github.com>
2018-04-18 17:38:37 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -4,8 +4,8 @@ class Ship():
     def __init__(self, screen):
         self.screen = screen
 
-        self.image = pygame.image.load("C:/Users/Xakep/Documents/GitHub/Codename-GAME-/images/ship.png")
-        self.image = pygame.transform.scale(self.image,(60,60))
+        self.image = pygame.image.load("images/player.png")
+        self.image = pygame.transform.scale(self.image,(80, 60))
         self.rect = self.image.get_rect()
         self.screen_rect = self.screen.get_rect()
 
@@ -14,19 +14,17 @@ class Ship():
 
         self.moving_right = False
         self.moving_left = False
-        self.moving_up = False
-        self.moving_down = False
+        
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.rect.centerx += 7
+            self.rect.centerx += 5
         if self.moving_left and self.rect.left > self.screen_rect.left:
-            self.rect.centerx -= 7
-        if self.moving_up and self.rect.top > self.screen_rect.top:
-            self.rect.bottom -= 7
-        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
-            self.rect.bottom += 7
+            self.rect.centerx -= 5
+        
+
+
 
