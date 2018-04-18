2983f2f5d13a40490c9db7323229b3013916321b
Bogdan Pahomov <36045528+VaultHack@users.noreply.github.com>
2018-04-18 17:40:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_function as g_f
-from pygame.sprite import Group
+from pygame.sprite import Group 
 
 def init_game():
     pygame.init()
@@ -11,13 +11,16 @@ def init_game():
     ship = Ship(screen)
     pygame.display.set_caption("Space-bound")
     bullets = Group()
-
-
+    meteors = Group()
+    
     while True:
         g_f.check_events(game_settings, ship, screen, bullets)
-        g_f.update_screen(screen, game_settings, ship, bullets)
+        g_f.update_screen(screen, game_settings, ship, bullets, meteors)
+        g_f.update_meteors(meteors, game_settings, screen)
         ship.update()
         bullets.update()
-
-
+        if len(meteors) == 0:
+            g_f.create_meteors(game_settings, screen, meteors)
+        collision = pygame.sprite.groupcollide(meteors, bullets, True, True)
+                   
 init_game()
